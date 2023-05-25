aa6f6dda740b9b4b108aab7982b71b169956a045
Svet_lana_17 <ocelot_17@mail.ru>
2023-05-25 23:38:15 +0600

    добавление localStorage

--- js/script.js
+++ js/script.js
@@ -4,28 +4,65 @@ const headerInput = document.querySelector('.header-input');
 const todoList = document.querySelector('.todo-list');
 const todoCompleted = document.querySelector('.todo-completed');
 
-const toDoData = [
-    {
-        text: 'кофе',
-        completed: false
-    },
-    {
-        text: 'кakao',
-        completed: true
-    }
+let toDoData = [];
 
-]
+
 const render = function () {
+    todoList.innerHTML = '';
+    todoCompleted.innerHTML = '';
+
+
+    toDoData = JSON.parse(localStorage.getItem('todoList'));
+    toDoData.forEach(function (item) {
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML = '<span class="text-todo">' + item.text + '</span>' +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            '</div>'
+        if (item.completed) {
+            todoCompleted.append(li)
+        } else {
+            todoList.append(li)
+        };
+        li.querySelector('.todo-complete').addEventListener('click', function () {
+            item.completed = !item.completed;
+            render();
+        })
+        li.querySelector('.todo-remove').addEventListener('click', function () {
+            toDoData.splice(item, 1);
+            render();
+        })
+    })
     console.log(toDoData);
 }
 todoControl.addEventListener('submit', function (event) {
     event.preventDefault();
-    const newToDo = {
-        text: headerInput.value,
-        completed: false
-    };
-    toDoData.push(newToDo);
-    console.log(headerInput.value);
-    render();
+    if (headerInput.value.trim() === '') {
+
+        headerInput.placeholder = 'заполните поле';
+    }
+    else {
+
+        const newToDo = {
+            text: headerInput.value,
+            completed: false
+        };
+        toDoData.push(newToDo);
+        //
+        localStorage.setItem('todoList', JSON.stringify(toDoData));
+        //
+        headerInput.value = '';
+        console.log(headerInput.value);
+        console.log(headerInput.value);
+        render();
+    }
+
 })
-console.log('hfh');
+
+
+
+
+
+
